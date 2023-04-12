c31e874e4957d832849640dbd4543e2373b33270
Ali Mert <alimert3215@gmail.com>
2023-04-12 05:30:12 +0300

    first commit

---
+++ project.js
@@ -0,0 +1,10 @@
+// ----------- Slot machine project-----------
+// 1 - Deposit some money
+// 2 - Determine number of lines to bet
+// 3 - Collect a bet amount
+// 4 - Spin the slot machine
+// 5 - Check if the player won
+// 6 - Pay the player
+// 7 - Repeat
+
+
